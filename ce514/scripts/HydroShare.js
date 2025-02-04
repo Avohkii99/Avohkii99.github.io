14a05f0746c63bafdae8743514c1fe1d78f0ab67
Avohkii99 <lesovikk8939@gmail.com>
2025-02-04 15:16:21 -0700

    More testing

--- ce514/scripts/HydroShare.js
+++ ce514/scripts/HydroShare.js
@@ -6,7 +6,7 @@ const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 L.tileLayer.wms('https://geoserver.hydroshare.org/geoserver/HS-0b3452c0c2d34f099e6da847a6ce828d/wms', {
-  layers: 'HS-a51c74b3a8734324a53bb2c29ae9df3b:Logannet',
+  layers: 'HS-a51c74b3a8734324a53bb2c29ae9df3b:Logan',
   format: 'image/png',
   transparent: true,
   attribution: 'Hydroshare GeoServer'
