357abb4a9133d6afda38838071d584ec11ff9949
Avohkii99 <lesovikk8939@gmail.com>
2025-02-06 09:10:06 -0700

    Style edits

--- ce514/scripts/HydroShare.js
+++ ce514/scripts/HydroShare.js
@@ -9,7 +9,7 @@ L.tileLayer.wms('https://geoserver.hydroshare.org/geoserver/HS-f6efb0188ee7472b8
   layers: 'HS-f6efb0188ee7472b8dda3a50af6cec56:YosemiteBound',
   format: 'image/png',
   transparent: true,
-  styles: 'line_black_heavy', // This style should be defined in the GeoServer to render the layer as a black, heavier line
+  styles: 'line', // This style should be defined in the GeoServer to render the layer as an outline
   attribution: 'Hydroshare GeoServer'
 }).addTo(map);
 
@@ -25,5 +25,6 @@ L.tileLayer.wms('https://geoserver.hydroshare.org/geoserver/HS-f6efb0188ee7472b8
   layers: 'HS-f6efb0188ee7472b8dda3a50af6cec56:POI',
   format: 'image/png',
   transparent: true,
+  styles: 'point', // This style should be defined in the GeoServer to render the layer as points
   attribution: 'Hydroshare GeoServer'
 }).addTo(map);
