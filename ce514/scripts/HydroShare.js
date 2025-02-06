9fdee7efd7dcf9ecbb9a03072c99bf0f5044c610
Avohkii99 <lesovikk8939@gmail.com>
2025-02-06 08:59:43 -0700

    Styling shift

--- ce514/scripts/HydroShare.js
+++ ce514/scripts/HydroShare.js
@@ -6,18 +6,20 @@ const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 L.tileLayer.wms('https://geoserver.hydroshare.org/geoserver/HS-f6efb0188ee7472b8dda3a50af6cec56/wms', {
+  layers: 'HS-f6efb0188ee7472b8dda3a50af6cec56:YosemiteBound',
+  format: 'image/png',
+  transparent: true,
+  styles: 'line', // This style should be defined in the GeoServer to render the layer as an outline
+  attribution: 'Hydroshare GeoServer'
+}).addTo(map);
+
+L.tileLayer.wms('https://geoserver.hydroshare.org/geoserver/HS-f6efb0188ee7472b8dda3a50af6cec56/wms', {
   layers: 'HS-f6efb0188ee7472b8dda3a50af6cec56:Trails',
   format: 'image/png',
   transparent: true,
   attribution: 'Hydroshare GeoServer'
 }).addTo(map);
 
-L.tileLayer.wms('https://geoserver.hydroshare.org/geoserver/HS-f6efb0188ee7472b8dda3a50af6cec56/wms', {
-  layers: 'HS-f6efb0188ee7472b8dda3a50af6cec56:YosemiteBound',
-  format: 'image/png',
-  transparent: true,
-  attribution: 'Hydroshare GeoServer'
-}).addTo(map);
 
 L.tileLayer.wms('https://geoserver.hydroshare.org/geoserver/HS-f6efb0188ee7472b8dda3a50af6cec56/wms', {
   layers: 'HS-f6efb0188ee7472b8dda3a50af6cec56:POI',
