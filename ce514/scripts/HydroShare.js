b2f0b5f11d5fc5d68d03ce6ef57df383d80e1e32
Avohkii99 <lesovikk8939@gmail.com>
2025-02-05 23:16:37 -0700

    More attempts

--- ce514/scripts/HydroShare.js
+++ ce514/scripts/HydroShare.js
@@ -1,4 +1,4 @@
-const map = L.map('map').setView([43.473956788279345, -111.93393322063366], 15);
+const map = L.map('map').setView([37.72901730242477, -119.64314080436989], 15);
 
 const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 20,
@@ -6,7 +6,7 @@ const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 L.tileLayer.wms('https://geoserver.hydroshare.org/geoserver/HS-f6efb0188ee7472b8dda3a50af6cec56/wms', {
-  layers: 'HS-f6efb0188ee7472b8dda3a50af6cec56:ViewQuality',
+  layers: 'HS-f6efb0188ee7472b8dda3a50af6cec56:Trails.xml',
   format: 'image/png',
   transparent: true,
   attribution: 'Hydroshare GeoServer'
