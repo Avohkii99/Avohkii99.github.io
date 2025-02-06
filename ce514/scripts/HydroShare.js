89f99cd06c0688e74159e6134898d92756c5ded1
Avohkii99 <lesovikk8939@gmail.com>
2025-02-06 08:40:32 -0700

    Updating JS to new file name for trails

--- ce514/scripts/HydroShare.js
+++ ce514/scripts/HydroShare.js
@@ -6,7 +6,7 @@ const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 L.tileLayer.wms('https://geoserver.hydroshare.org/geoserver/HS-f6efb0188ee7472b8dda3a50af6cec56/wms', {
-  layers: 'HS-f6efb0188ee7472b8dda3a50af6cec56:Trails.xml',
+  layers: 'HS-f6efb0188ee7472b8dda3a50af6cec56:Trails in Yosemite NP',
   format: 'image/png',
   transparent: true,
   attribution: 'Hydroshare GeoServer'
