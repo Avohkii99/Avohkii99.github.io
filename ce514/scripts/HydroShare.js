d3ec37c3724223e0a572c6132d2706fdade46754
Avohkii99 <lesovikk8939@gmail.com>
2025-02-06 08:55:53 -0700

    Adding all the files

--- ce514/scripts/HydroShare.js
+++ ce514/scripts/HydroShare.js
@@ -6,7 +6,21 @@ const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 L.tileLayer.wms('https://geoserver.hydroshare.org/geoserver/HS-f6efb0188ee7472b8dda3a50af6cec56/wms', {
-  layers: 'HS-f6efb0188ee7472b8dda3a50af6cec56:Trails in Yosemite NP',
+  layers: 'HS-f6efb0188ee7472b8dda3a50af6cec56:Trails',
+  format: 'image/png',
+  transparent: true,
+  attribution: 'Hydroshare GeoServer'
+}).addTo(map);
+
+L.tileLayer.wms('https://geoserver.hydroshare.org/geoserver/HS-f6efb0188ee7472b8dda3a50af6cec56/wms', {
+  layers: 'HS-f6efb0188ee7472b8dda3a50af6cec56:YosemiteBound',
+  format: 'image/png',
+  transparent: true,
+  attribution: 'Hydroshare GeoServer'
+}).addTo(map);
+
+L.tileLayer.wms('https://geoserver.hydroshare.org/geoserver/HS-f6efb0188ee7472b8dda3a50af6cec56/wms', {
+  layers: 'HS-f6efb0188ee7472b8dda3a50af6cec56:POI',
   format: 'image/png',
   transparent: true,
   attribution: 'Hydroshare GeoServer'
