e87a1ea7434e36ebc1fb7509596cab19532e7e97
Avohkii99 <lesovikk8939@gmail.com>
2025-02-06 11:12:30 -0700

    Adding POI to list

--- ce514/scripts/HydroShare.js
+++ ce514/scripts/HydroShare.js
@@ -39,9 +39,13 @@ const layers = {
     format: 'image/png',
     transparent: true,
     attribution: 'Hydroshare GeoServer'
-  }).on('load', function() {
-    const layer = this.getContainer();
-    layer.style.filter = 'invert(1) grayscale(1) brightness(0)';
+  }),
+
+  POI: L.tileLayer.wms('https://geoserver.hydroshare.org/geoserver/HS-f6efb0188ee7472b8dda3a50af6cec56/wms', {
+    layers: 'HS-f6efb0188ee7472b8dda3a50af6cec56:POI',
+    format: 'image/png',
+    transparent: true,
+    attribution: 'Hydroshare GeoServer'
   })
 };
 
